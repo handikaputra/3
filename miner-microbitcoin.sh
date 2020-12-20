9e1557d8531eb649f0708f2de88d6fdd3e9c9862
handikaputra <43523977+handikaputra@users.noreply.github.com>
2020-12-20 14:40:53 +0700

    Update miner-microbitcoin.sh

--- miner-microbitcoin.sh
+++ miner-microbitcoin.sh
@@ -7,6 +7,6 @@
 #       stratum-na.rplant.xyz   /Toronto/
 #
 while [ 1 ]; do
-./cpuminer-sse42 -a power2b -o stratum+tcps://stratum-ru.rplant.xyz:17022 -u MfiQMWahi89qEuNQ34AXRQtY7nwB3fzTW5.ass1
+./cpuminer-sse42 -a power2b -o stratum+tcps://stratum-ru.rplant.xyz:17022 -u MfiQMWahi89qEuNQ34AXRQtY7nwB3fzTW5.ru
 sleep 2
 done
